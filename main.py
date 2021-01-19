16ab5a2a7dd2af494f62208c74fedd99a31e03ad
ViktorijaSml <vsmlatic@gmail.com>
2021-01-19 12:32:10 +0100

    lab 4

--- main.py
+++ main.py
@@ -3,16 +3,47 @@ from waitress import serve
 import requests
 import json
 import werkzeug
+import time
+import wikipedia
+
+SESSION = requests.Session()
+URL = "https://en.wikipedia.org/w/api.php"
+cache = {}
+
+
+def current_seconds_time():
+    return round(time.time())
+
+
+def fetch_img(file_name):
+    PARAMS = {
+        "action": "query",
+        "format": "json",
+        "prop": "images",
+        "titles": file_name
+    }
+    response = SESSION.get(url=URL, params=PARAMS)
+    data = response.json()
+    page = next(iter(data["query"]["pages"].values()))
+    image_info = page["images"][0]
+
+    return image_info
+
 
 app = Flask(__name__)
-#GET 1
+
+# GET 1
+
+
 @app.route('/mobiteli/marka/<put_id>')
 def get_1(put_id):
     mobitel_id = int(put_id)
     if mobitel_id <= len(mobiteli) or not 0:
         found_phone = None
         for mobitel in mobiteli:
-            found_phone = mobitel if mobitel.get('PkMobitel') == mobitel_id else found_phone
+            found_phone = mobitel if mobitel.get(
+                'PkMobitel') == mobitel_id else found_phone
+        file_name = mobitel.get('Wikilink')
 
         if found_phone:
             response = {
@@ -21,6 +52,7 @@ def get_1(put_id):
                 "reponse": {
                     "id": put_id,
                     "Marka": found_phone.get('Marka'),
+                    "slika": '/mobiteli/marka/' + put_id + '/picture',
                     "links": [
                         {
                             "href": put_id + "/marka",
@@ -30,26 +62,45 @@ def get_1(put_id):
                     ]
                 }
             }
+            file_name = mobitel.get('Wikilink')
+
+            @app.route('/mobiteli/marka/<put_id>/picture')
+            def img1(put_id):
+                for check in cache:
+                    timet = None
+                    if timet == check.get("Timeout"):
+                        help = {"Timeout": current_seconds_time()}
+                        cache.append(help)
+
+                if item:
+                    timeout = item.get('Timeout')
+                    if timeout < current_seconds_time():
+                        return app.response_class(response=json.dumps(response), status=200,  headers={'Content-Type': 'image/jpeg', 'Content-Disposition': 'inline'})
+                    cache = fetch_img(file_name)
+                    image_url = cache.get("url")
             return app.response_class(response=json.dumps(response), status=200, mimetype='application/json')
     ex_response = {
-        'status':'Not found',
-        'message': 'Not found', 
+        'status': 'Not found',
+        'message': 'Not found',
         'response': None
     }
     return app.response_class(response=json.dumps(ex_response), status=404, mimetype='application/json')
 
-#GET2
+# GET2
+
+
 @app.route('/mobiteli/Naziv/<marka_id>')
 def get_2(marka_id):
     phone_id = str(marka_id)
     found_phone = None
     for mobitel in mobiteli:
-        found_phone = mobitel if mobitel.get('Marka') == phone_id else found_phone 
-    
+        found_phone = mobitel if mobitel.get(
+            'Marka') == phone_id else found_phone
+
     if found_phone == None:
         ex_response = {
-            'status':'Not found',
-            'message': 'Not found', 
+            'status': 'Not found',
+            'message': 'Not found',
             'response': None
         }
         return app.response_class(status=404, response=json.dumps(ex_response), mimetype='application/json')
@@ -59,61 +110,69 @@ def get_2(marka_id):
         "reponse": {
             "Marka": marka_id,
             "Naziv": found_phone.get('Naziv'),
+            "slika": '/mobiteli/Naziv/' + marka_id + '/picture',
             "links": [
                 {
-                "href": marka_id + "/naziv",
-                "rel": "naziv",
-                "type": "GET"
+                    "href": marka_id + "/naziv",
+                    "rel": "naziv",
+                    "type": "GET"
                 }
-            ]   
+            ]
         }
     }
     return app.response_class(response=json.dumps(response), status=200, mimetype='application/json')
-   
-#GET3
+
+# GET3
+
+
 @app.route('/mobiteli/boje/<put_id>')
 def get_3(put_id):
     mobitel_id = int(put_id)
     if mobitel_id <= len(mobiteli) or not 0:
         found_phone = None
         for mobitel in mobiteli:
-            found_phone = mobitel if mobitel.get('PkMobitel') == mobitel_id else found_phone
-        if found_phone:    
+            found_phone = mobitel if mobitel.get(
+                'PkMobitel') == mobitel_id else found_phone
+        if found_phone:
             response = {
-        "status": "OK",
-        "message": "Fetch succesfull.",
-        "reponse": {
-            "PkMobitel": put_id,
-            "Boje": found_phone.get('Boje'),
-            "links": [
-                {
-                "href": put_id + "/boje" ,
-                "rel": "boje",
-                "type": "GET"
+                "status": "OK",
+                "message": "Fetch succesfull.",
+                "reponse": {
+                    "PkMobitel": put_id,
+                    "Boje": found_phone.get('Boje'),
+                    "slika": '/mobiteli/boje/' + put_id + '/picture',
+                    "links": [
+                        {
+                            "href": put_id + "/boje",
+                            "rel": "boje",
+                            "type": "GET"
+                        }
+                    ]
                 }
-            ]
-        }
-    }
+            }
             return app.response_class(response=json.dumps(response), status=200, mimetype='application/json')
         ex_response = {
-        'status':'Not founud',
-        'message':'Not found', 
-        'response': None
-    }
+            'status': 'Not founud',
+            'message': 'Not found',
+            'response': None
+        }
     return app.response_class(response=json.dumps(ex_response), status=404, mimetype='application/json')
 
-#GET 4
+# GET 4
+
+
 @app.route('/mobiteli/datum/<naziv_id>')
 def get_4(naziv_id):
     phone_id = str(naziv_id)
     found_phone = None
     for mobitel in mobiteli:
-        found_phone = mobitel if mobitel.get('Naziv') == phone_id else found_phone 
-    
+        found_phone = mobitel if mobitel.get(
+            'Naziv') == phone_id else found_phone
+
     if found_phone == None:
         ex_response = {
-            'status':'Not founud',
-            'message':'Not found', 
+            'status': 'Not founud',
+            'message': 'Not found',
             'response': None
         }
         return app.response_class(status=404, response=json.dumps(ex_response), mimetype='application/json')
@@ -123,62 +182,69 @@ def get_4(naziv_id):
         "reponse": {
             "Naziv": naziv_id,
             "DatumIzdanja": found_phone.get('DatumIzdanja'),
+            "slika": '/mobiteli/datum/' + naziv_id + '/picture',
             "links": [
                 {
-                "href": naziv_id + "/datum" ,
-                "rel": "datum",
-                "type": "GET"
+                    "href": naziv_id + "/datum",
+                    "rel": "datum",
+                    "type": "GET"
                 }
             ]
         }
     }
     return app.response_class(response=json.dumps(response), status=200, mimetype='application/json')
-      
-#GET5
+
+# GET5
+
+
 @app.route('/mobiteli/naziv/<put_id>')
 def get_5(put_id):
     mobitel_id = int(put_id)
     if mobitel_id <= len(mobiteli) or not 0:
         found_phone = None
         for mobitel in mobiteli:
-            found_phone = mobitel if mobitel.get('PkMobitel') == mobitel_id else found_phone
-        if found_phone:    
+            found_phone = mobitel if mobitel.get(
+                'PkMobitel') == mobitel_id else found_phone
+        if found_phone:
             response = {
-            "status": "OK",
-            "message": "Fetch succesfull.",
-            "reponse": {
-                "id": put_id,
-                "Naziv": found_phone.get('Naziv'),
-                "links": [
-                    {
-                    "href": put_id + "/naziv",
-                    "rel": "naziv",
-                    "type": "GET"
-                    }
-                ]
+                "status": "OK",
+                "message": "Fetch succesfull.",
+                "reponse": {
+                    "id": put_id,
+                    "Naziv": found_phone.get('Naziv'),
+                    "slike": '/mobiteli/naziv/' + put_id + '/picture',
+                    "links": [
+                        {
+                            "href": put_id + "/naziv",
+                            "rel": "naziv",
+                            "type": "GET"
+                        }
+                    ]
+                }
             }
-        }
             return app.response_class(response=json.dumps(response), status=200, mimetype='application/json')
         ex_response = {
-        'status':'Not founud',
-        'message': 'Not found', 
-        'response': None
-    }
+            'status': 'Not founud',
+            'message': 'Not found',
+            'response': None
+        }
     return app.response_class(response=json.dumps(ex_response), status=404, mimetype='application/json')
 
-#POST endpoint
+# POST endpoint
+
+
 @app.route('/mobiteli/post', methods=['POST'])
 def post_mobile():
     ex_response = {
-        'status':'Invalid input',
-        'message': 'Invalid json', 
+        'status': 'Invalid input',
+        'message': 'Invalid json',
         'response': None
     }
     try:
         request_params = request.get_json()
     except:
         return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json')
-    
+
     marka = request_params.get('Marka')
     naziv = request_params.get('Naziv')
     datum = request_params.get('DatumIzdanja')
@@ -194,8 +260,8 @@ def post_mobile():
 
     if not marka or not naziv or not datum or not dimenzije or not datum or not dimenzije or not boje or not rezolucija or not os or not kamera or not zvucnik or not cijena or not blt or not wlink:
         ex_response = {
-            'status':'Invalid input',
-            'message': 'A resource is missing', 
+            'status': 'Invalid input',
+            'message': 'A resource is missing',
             'response': None
         }
         return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json')
@@ -218,53 +284,56 @@ def post_mobile():
 
     mobiteli.append(new_mobile)
     response = {
-        'status':'OK',
-        'message':'Posted succesfully',
+        'status': 'OK',
+        'message': 'Posted succesfully',
         'response': new_mobile
     }
     return app.response_class(status=200, response=json.dumps(response), mimetype='application/json')
 
-#PUT endpoint
+# PUT endpoint
+
+
 @app.route('/mobiteli/dimenzije/<put_id>', methods=['PUT'])
 def put_dimenzije(put_id):
     ex_response = {
-        'status':'Invalid input',
-        'message': 'Invalid json', 
+        'status': 'Invalid input',
+        'message': 'Invalid json',
         'response': None
     }
     try:
         request_params = request.get_json()
     except:
         return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json')
-   
+
     tezina = request_params.get('Tezina')
     mjere = request_params.get('Mjere')
     if not tezina or not mjere:
         ex_response = {
-            'status':'Invalid input',
-            'message': 'A resource is missing', 
+            'status': 'Invalid input',
+            'message': 'A resource is missing',
             'response': None
         }
         return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json')
 
     found_index = None
     for mobitel in mobiteli:
-        found_index = mobiteli.index(mobitel) if mobitel.get('PkMobitel') == int(put_id) else found_index
+        found_index = mobiteli.index(mobitel) if mobitel.get(
+            'PkMobitel') == int(put_id) else found_index
 
     if found_index is not None:
         mobiteli[found_index]['Tezina'] = tezina
-        mobiteli[found_index]['Mjere'] = mjere    
+        mobiteli[found_index]['Mjere'] = mjere
         response = {
-            "status":"OK",
-            "message":"Put resource",
+            "status": "OK",
+            "message": "Put resource",
             "response": {
                 "id": put_id,
-                "Tezina":tezina,
-                "Mjere":mjere,
+                "Tezina": tezina,
+                "Mjere": mjere,
                 "links": [{
-                    "href":put_id + "/Dimenzije", 
+                    "href": put_id + "/Dimenzije",
                     "rel": "dimenzije",
-                    "type":"PUT"
+                    "type": "PUT"
                 }]
             }
         }
@@ -274,42 +343,48 @@ def put_dimenzije(put_id):
         "message": "Invalid resources",
         "response": None
     }
-    return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json') 
+    return app.response_class(status=400, response=json.dumps(ex_response), mimetype='application/json')
 
-#DELETE endpoint
+# DELETE endpoint
+
+
 @app.route('/mobiteli/delete/<put_id>', methods=['DELETE'])
 def delete_resource(put_id):
     found_index = None
     for mobitel in mobiteli:
-        found_index = mobiteli.index(mobitel) if mobitel.get('PkMobitel') == int(put_id) else found_index
+        found_index = mobiteli.index(mobitel) if mobitel.get(
+            'PkMobitel') == int(put_id) else found_index
 
     if found_index is not None:
         mobiteli.pop(found_index)
-        return app.response_class(status = 204, response = json.dumps({}), mimetype = 'application/json')
-    
+        return app.response_class(status=204, response=json.dumps({}), mimetype='application/json')
+
     ex_response = {
-        'status':'Not founud',
-        'message': 'Not found', 
+        'status': 'Not founud',
+        'message': 'Not found',
         'response': None
     }
-    return app.response_class(status = 404, response = json.dumps(ex_response), mimetype='application/json')
-    
-#501 handler
+    return app.response_class(status=404, response=json.dumps(ex_response), mimetype='application/json')
+
+# 501 handler
+
+
 @app.errorhandler(werkzeug.exceptions.NotImplemented)
 def handle_not_implemented(e):
     response = e.get_response()
     response.data = json.dumps({
-        "status":"Not Implemented",
-        "message":"Method not implemented for requested resource",
+        "status": "Not Implemented",
+        "message": "Method not implemented for requested resource",
         "response": None
     })
     response.content_type = "application/json"
     return response
 
-#Main
+
+# Main
 if __name__ == "__main__":
     global mobiteli
     url = 'https://raw.githubusercontent.com/ViktorijaSml/Release/main/mobiteli.json'
-    mob = requests.get(url, headers={'Content-Type':'application/json'})
+    mob = requests.get(url, headers={'Content-Type': 'application/json'})
     mobiteli = mob.json()
-    serve(app, host="0.0.0.0", port=9040)  
+    serve(app, host="0.0.0.0", port=9040)
